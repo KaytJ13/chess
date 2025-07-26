496824bececede061839aef7c3e13b22f17ab580
Kate Norcross <130419451+KaytJ13@users.noreply.github.com>
2025-07-25 21:32:08 -0600

    Login, Register, Clear junit tests

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -3,10 +3,7 @@ package client;
 import exception.ResponseException;
 import model.AuthData;
 import org.junit.jupiter.api.*;
-import requests.CreateGameRequest;
-import requests.ListGamesResponse;
-import requests.RegisterRequest;
-import requests.UserFriendlyGameData;
+import requests.*;
 import server.Server;
 import server.ServerFacade;
 
@@ -28,9 +25,56 @@ public class ServerFacadeTests {
 
     @AfterAll
     static void stopServer() {
+        try {
+            facade.clear();
+        } catch (ResponseException e) {
+            return;
+        }
         server.stop();
     }
 
+
+    @Test
+    public void testRegisterPositive() {
+        try {
+            AuthData auth = facade.register(new RegisterRequest("ger", "ger", "ger@ld.com"));
+            auth = facade.login(new LoginRequest("ger", "ger"));
+            assert auth != null;
+        } catch (Exception e) {
+            assert false;
+        }
+    }
+
+    @Test
+    public void testRegisterNegative() {
+        try {
+            facade.register(new RegisterRequest(null, "ger", "ger"));
+            assert false;
+        } catch (Exception e) {
+            assert true;
+        }
+    }
+
+    @Test
+    public void testLoginPositive() {
+        try {
+            AuthData auth = facade.register(new RegisterRequest("Amy", "march", "artist@com"));
+            facade.login(new LoginRequest("Amy", "march"));
+            assert true;
+        } catch (Exception e) {
+            assert false : e.getMessage();
+        }
+    }
+
+    @Test
+    public void testLoginNegative() {
+        try {
+            facade.login(new LoginRequest("Amy", "march"));
+            assert true;
+        } catch (Exception e) {
+            assert false : e.getMessage();
+        }
+    }
 
     @Test
     public void testClear() {
@@ -40,8 +84,10 @@ public class ServerFacadeTests {
             facade.clear();
             ListGamesResponse expected = new ListGamesResponse(new UserFriendlyGameData[0]);
             assert Objects.equals(facade.listGames(auth.authToken()), expected);
+            auth = facade.login(new LoginRequest("ger", "ger"));
+            assert false : auth.username();
         } catch (ResponseException e) {
-            assert false : e.getMessage();
+            assert true : e.getMessage();
         }
     }
 
